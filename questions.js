5b26774d809ee63c597dce0fcc9f40ad5aa30470
ANEEQ - UR - RAHMAN KHAN <aneeqok@yahoo.com>
2017-01-28 03:52:19 +0500

    first commit

---
+++ questions.js
@@ -0,0 +1,47 @@
+var questions = [
+{
+    "question" : 'A device that converts digital signals to analog signals is ?' ,
+    "option1" : 'Packet' ,
+    "option2" : 'Modem' ,
+    "option3" : 'HDD' ,
+    "option4" : 'RAM',
+    "answer"  : "2"
+},
+
+{
+    "question" : 'Which of the following is NOT operating system?' ,
+    "option1" : 'Windows' ,
+    "option2" : 'Linux' ,
+    "option3" : 'DOS' ,
+    "option4" : 'Java',
+    "answer"  : "4"
+},
+
+{
+    question : ' Who is father of modern computers?' ,
+    "option1" : 'Abraham Lincoln' ,
+    "option2" : 'James Goslinga' ,
+    "option3" : 'Charles Babbage' ,
+    "option4" : 'Gordon E. Moore',
+    "answer"  : "3"
+},
+
+{
+    
+    "question" : 'LAN means' ,
+    "option1" : 'Least Area Network' ,
+    "option2" : 'Large Area Network' ,
+    "option3" : 'Light Area Network' ,
+    "option4" : 'Local Area Network',
+    "answer"  : "4"
+},
+
+{
+    "question" : 'Java is a ' ,
+    "option1" : 'Programming Language' ,
+    "option2" : 'Operating System' ,
+    "option3" : 'Compiler' ,
+    "option4" : 'Input Device',
+    "answer"  : "1"
+}
+]
